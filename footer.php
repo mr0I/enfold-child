4afc6841430bf2ad0bc602591cea0b915b701241
Unco <wizard2070@gmail.com>
2021-01-09 12:15:57 +0330

    changed floated shop icon styles and more

--- footer.php
+++ footer.php
@@ -412,8 +412,8 @@ if(isset($avia_config['fullscreen_image']))
 
 <!-- custom code -->
 <div data-toggle="tooltip" data-placement="top" title="ورود به فروشگاه">
-    <a class="floated-shop" href="https://radshid.com/shop-%d9%81%d8%b1%d9%88%d8%b4%da%af%d8%a7%d9%87-%d8%b1%d8%af%db%8c%d8%a7%d8%a8-%d8%ae%d9%88%d8%af%d8%b1%d9%88-%d9%88-%d8%b4%d8%ae%d8%b5%db%8c-%d8%b1%d8%a7%d8%af%d8%b4%db%8c%d8%af/"  target="_blank">
-        <svg id="i-bag" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+    <a class="floated-shop" href="https://radshid.com/shop/"  target="_blank">
+        <svg id="i-bag" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
             <path d="M5 9 L5 29 27 29 27 9 Z M10 9 C10 9 10 3 16 3 22 3 22 9 22 9" />
         </svg>
     </a>
