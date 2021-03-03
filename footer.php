99bc9dd36d98c5dafb43fc10c898dd130c8d5888
Unco <wizard2070@gmail.com>
2021-03-03 15:55:54 +0330

    changed css 2 sass

--- footer.php
+++ footer.php
@@ -405,7 +405,7 @@ if(isset($avia_config['fullscreen_image']))
 
 <!-- custom code -->
 <div>
-    <a class="floated-shop" href="https://radshid.com/shop/"  target="_blank" title="ورود به فروشگاه">
+    <a class="floated-shop" href="https://radshid.com/shop/" title="ورود به فروشگاه">
         <svg id="i-bag" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="30" height="30" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
             <path d="M5 9 L5 29 27 29 27 9 Z M10 9 C10 9 10 3 16 3 22 3 22 9 22 9" />
         </svg>
@@ -447,7 +447,7 @@ if(isset($avia_config['fullscreen_image']))
             <img src="<?php echo get_stylesheet_directory_uri().'/images/insta.png' ; ?>" alt="اینستاگرام">
             <span>اینستاگرام</span>
         </a>
-        <a href="https://radshid.com/?p=10008" target="_blank" title="ورود به فروشگاه">
+        <a href="https://radshid.com/?p=10008" title="ورود به فروشگاه">
             <img src="<?php echo get_stylesheet_directory_uri().'/images/shop.png' ; ?>" alt="ورود به فروشگاه">
             <span>فروشگاه</span>
         </a>
--- functions.php
+++ functions.php
@@ -13,10 +13,7 @@ defined("ABSPATH") || exit();
 function load_scripts_styles() {
 	$vn = wp_get_theme()->get( 'Version' );
 	// Styles
-	wp_enqueue_style( 'maticons' , get_stylesheet_directory_uri() . '/css/maticons.css' );
-	wp_enqueue_style( 'wpforms' , get_stylesheet_directory_uri() . '/css/wpforms.css');
-	wp_enqueue_style( 'custom-styles' , get_stylesheet_directory_uri() . '/css/custom-styles.css');
-	wp_enqueue_style( 'front-styles' , get_stylesheet_directory_uri() . '/css/front-styles.css');
+	wp_enqueue_style( 'child-theme-styles' , get_stylesheet_directory_uri() . '/css/main.css');
 	// Scripts
 	wp_enqueue_script( 'custom-scripts', get_stylesheet_directory_uri() . '/js/custom-scripts.js', array('jquery'), $vn , true );
 	wp_localize_script( 'custom-scripts', 'SpaAjax', array(
@@ -62,55 +59,6 @@ function product_tags($attr , $content){
 }
 
 
-/**
- * Add extra fields to register form.
- */
-//function wooc_extra_register_fields() {?>
-<!--	<p class="form-row form-row-first">-->
-<!--		<label for="reg_billing_first_name">--><?php //_e( 'First name', 'woocommerce' ); ?><!--</label>-->
-<!--		<input type="text" class="input-text" name="billing_first_name" id="reg_billing_first_name"-->
-<!--		       value="--><?php //if ( ! empty( $_POST['billing_first_name'] ) ) esc_attr_e( $_POST['billing_first_name'] ); ?><!--" />-->
-<!--	</p>-->
-<!--	<p class="form-row form-row-last">-->
-<!--		<label for="reg_billing_last_name">--><?php //_e( 'Last name', 'woocommerce' ); ?><!--</label>-->
-<!--		<input type="text" class="input-text" name="billing_last_name" id="reg_billing_last_name"-->
-<!--		       value="--><?php //if ( ! empty( $_POST['billing_last_name'] ) ) esc_attr_e( $_POST['billing_last_name'] ); ?><!--" />-->
-<!--	</p>-->
-<!--	<p class="form-row form-row-wide">-->
-<!--		<label for="reg_billing_phone">--><?php //_e( 'Phone Number', 'woocommerce' ); ?><!--</label>-->
-<!--		<input type="text" class="input-text" name="billing_phone" id="reg_billing_phone"-->
-<!--		       value="--><?php //if ( ! empty( $_POST['billing_phone'] ) ) esc_attr_e( $_POST['billing_phone'] ); ?><!--" maxlength="11"-->
-<!--		       onkeyup="this.value = this.value.replace(/[^\d\.]+/g, '');"/>-->
-<!--	</p>-->
-<!--	<div class="clear"></div>-->
-<!--	--><?php
-//}
-//add_action( 'woocommerce_register_form_start', 'wooc_extra_register_fields' );
-/**
- * Below code save extra fields.
- */
-//function wooc_save_extra_register_fields( $customer_id ) {
-//	if ( isset( $_POST['billing_phone'] ) ) {
-//		// Phone input filed which is used in WooCommerce
-//		update_user_meta( $customer_id, 'billing_phone', sanitize_text_field( $_POST['billing_phone'] ) );
-//	}
-//	if ( isset( $_POST['billing_first_name'] ) ) {
-//		//First name field which is by default
-//		update_user_meta( $customer_id, 'first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
-//		// First name field which is used in WooCommerce
-//		update_user_meta( $customer_id, 'billing_first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
-//	}
-//	if ( isset( $_POST['billing_last_name'] ) ) {
-//		// Last name field which is by default
-//		update_user_meta( $customer_id, 'last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
-//		// Last name field which is used in WooCommerce
-//		update_user_meta( $customer_id, 'billing_last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
-//	}
-//}
-//add_action( 'woocommerce_created_customer', 'wooc_save_extra_register_fields' );
-
-
-
 /*
  * Replace 'textdomain' with your plugin's textdomain. e.g. 'woocommerce'.
  * File to be named, for example, yourtranslationfile-en_GB.mo
@@ -153,33 +101,6 @@ add_shortcode('avs_posts_tag', 'avs_posts_tag_cb');
 //add_action( 'woocommerce_init', 'wc_remove_product_schema_product_archive' );
 
 
-// Redirect wp-login.php
-//function redirect_to_nonexistent_page(){
-//	$new_login=  'my-account';
-//	if(strpos($_SERVER['REQUEST_URI'], $new_login) === false){
-//		wp_safe_redirect( home_url(  ) );
-//		exit();
-//	}
-//}
-//add_action( 'login_head', 'redirect_to_nonexistent_page');
-//function redirect_to_actual_login(){
-//	$new_login =  'radcustomsignin';
-//	if(parse_url($_SERVER['REQUEST_URI'],PHP_URL_QUERY) == $new_login&& ($_GET['redirect'] !== false)){
-//		wp_safe_redirect(home_url("wp-login.php?$new_login&redirect=false"));
-//		exit();
-//	}
-//}
-//add_action( 'init', 'redirect_to_actual_login');
-// Redirect wp-login.php
-
-
-
-////add text note to product description page for all downloadable products
-//function append_download_note() {
-//		echo '<p><a href="https://eeeico.com/contact-us/">جهت سفارش لطفا با ما تماس بگیرید</a></p>';
-//}
-//add_action( 'woocommerce_before_add_to_cart_button', 'append_download_note', 10, 0 );
-
 
 
 add_action('wp_logout','auto_redirect_external_after_logout');
@@ -189,34 +110,9 @@ exit();
 }
 
 
-//function add_cron_recurrence_interval( $schedules ) {
-//	$schedules['every_ten_minutes'] = array(
-//		'interval'  => 60,
-//		'display'   => __( 'هر 10 دقیقه', 'uap' )
-//	);
-//	return $schedules;
-//}
-//add_filter( 'cron_schedules', 'add_cron_recurrence_interval' );
-
-
-
 /* Pro Ranks Schedule */
 //wp_schedule_event( time(), '', 'uapDoRanksReset');//modify time
 //wp_schedule_event( time(), 'every_ten_minutes', 'uap_cron_job');//modify time
-
-
-
-/*
-* Rich Snippet Data
-* Add missing data not handled by WooCommerce yet - Webjame.Com
-*/
-function custom_woocommerce_structured_data_product ($data) {
-	global $product;
-	$data['brand'] = $product->get_attribute('brand') ? $product->get_attribute('brand') : null;
-	$data['mpn'] = $product->get_sku() ? $product->get_sku() : null;
-	return $data;
-}
-add_filter( 'woocommerce_structured_data_product', 'custom_woocommerce_structured_data_product' );
 
 
 
