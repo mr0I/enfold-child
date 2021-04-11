8bd36e0d8159d5fe4e3a8484e69b1e5b520f853b
Unco <wizard2070@gmail.com>
2021-04-11 14:53:56 +0430

    changes on product template

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -41,7 +41,7 @@ jQuery(document).ready(function($){
 
 
     // Add Material icons
-    $('.single_add_to_cart_button').html('<i class="ic-basket my-2"></i>  افزودن به سبد خرید');
+    $('.single_add_to_cart_button').html('<i class="ic-cart"></i>  افزودن به سبد خرید');
     $('.cart_dropdown_link').find('span:first-child').html('<i class="ic-cart my-2"></i>');
 
 
@@ -64,7 +64,6 @@ jQuery(document).ready(function($){
         }
     } , 300);
 
-
     // Change Sipaad phone number
     $('#product-14049').find('.woo-price').find('strong').html('برای دریافت قیمت با شماره 03132362894 تماس بگیرید');
     $('.template-shop').find('.post-14049').find('.price').find('strong').html('برای دریافت قیمت با شماره 03132362894 تماس بگیرید');
@@ -76,8 +75,7 @@ jQuery(document).ready(function($){
     $('.wpsp-cart-button').find('a.add_to_cart_button').html(' <i class="ic-chevron-left" style="vertical-align: middle;"></i> خرید');
 
     // Customize woocommerce product desc
-    $('.woocommerce-product-details__short-description').before("<p style='font-size: 15px;font-weight: bold;padding: 4px 0;margin: 0;'><i style='font-size: 1.5em;' class='ic-dot-single align-middle font-weight-bold'></i> ویژگی ها</p>");
-
+    $('.woocommerce-product-details__short-description').before("<p style='font-size: 15px;font-weight: bold;padding: 4px 0;margin: 0;'>ویژگی ها</p>");
 });
 
 
