68aa4183eac3bc40ee9411b4957379d0a26c1184
Unco <wizard2070@gmail.com>
2021-04-12 13:02:15 +0430

    chnages on login/register btn

--- footer.php
+++ footer.php
@@ -542,6 +542,14 @@ if(isset($avia_config['fullscreen_image']))
 
 
 if (is_user_logged_in()){
+    ?>
+    <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            let my_account_elm = $('li.menu-item.menu-item-top-level-8');
+            my_account_elm.find('span.avia-menu-text').html('<i class="ic-user1"></i>حساب کاربری');
+        })
+    </script>
+    <?php
 	$user_id = get_current_user_id();
 	$user = get_userdata($user_id);
 	$user_roles = $user->roles;
@@ -549,9 +557,9 @@ if (is_user_logged_in()){
         // Append extra element after Agent login
         ?>
         <script type="text/javascript">
-        jQuery(document).ready(function($) {
-            $('#avia-menu').append('<li id="menu-item-999" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-top-level menu-item-top-level-1"><a href="<?php echo get_option('RADtools_setting_agencies_orders_list_link'); ?>" itemprop="url"><span class="avia-bullet"></span><span class="avia-menu-text"><i class="fa fa-home"></i><?php echo __('Bulk Orders', 'radshid_lan'); ?></span><span class="avia-menu-fx"><span class="avia-arrow-wrap"><span class="avia-arrow"></span></span></span></a></li>');
-        })
+            jQuery(document).ready(function($) {
+                $('#avia-menu').append('<li id="menu-item-999" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-top-level menu-item-top-level-1"><a href="<?php echo get_option('RADtools_setting_agencies_orders_list_link'); ?>" itemprop="url"><span class="avia-bullet"></span><span class="avia-menu-text"><i class="fa fa-home"></i><?php echo __('Bulk Orders', 'radshid_lan'); ?></span><span class="avia-menu-fx"><span class="avia-arrow-wrap"><span class="avia-arrow"></span></span></span></a></li>');
+            })
         </script>
         <?php
     }
@@ -563,6 +571,8 @@ if (is_user_logged_in()){
             if(elm == null){
                 $('#avia-menu').remove('<li id="menu-item-999"</li>');
             }
+            let my_account_elm = $('li.menu-item.menu-item-top-level-8');
+            my_account_elm.find('span.avia-menu-text').html('<i class="ic-user1"></i>ورود / ثبت نام');
         })
     </script>
 <?php
