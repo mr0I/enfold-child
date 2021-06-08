249bd275202ecaa5a59c3fc0cd7c240b62852874
Unco <wizard2070@gmail.com>
2021-06-08 15:37:21 +0430

    Added Recaptcah in single.php

--- functions.php
+++ functions.php
@@ -316,3 +316,39 @@ function add_defer_attribute($tag, $handle) {
 	return $tag;
 }
 
+
+/* Start Recaptcha */
+function add_google_recaptcha($submit_field) {
+	$submit_field['submit_field'] = '<div class="g-recaptcha" data-sitekey="6LfZS6gZAAAAAC5BMOsbcNQYDqtQm6puHeUmjlid"></div><br>' . $submit_field['submit_field'];
+	return $submit_field;
+}
+if (!is_user_logged_in()) {
+	add_filter('comment_form_defaults','add_google_recaptcha');
+}
+function is_valid_captcha($captcha) {
+	$captcha_postdata = http_build_query(array(
+		'secret' => '6LfZS6gZAAAAAKtbN3kOXbdrjyZZcKUwt8ondcKE',
+		'response' => $captcha,
+		'remoteip' => $_SERVER['REMOTE_ADDR']));
+	$captcha_opts = array('http' => array(
+		'method'  => 'POST',
+		'header'  => 'Content-type: application/x-www-form-urlencoded',
+		'content' => $captcha_postdata));
+	$captcha_context  = stream_context_create($captcha_opts);
+	$captcha_response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify" , false , $captcha_context), true);
+	if ($captcha_response['success'])
+		return true;
+	else
+		return false;
+}
+function verify_google_recaptcha() {
+	$recaptcha = $_POST['g-recaptcha-response'];
+	if (empty($recaptcha))
+		wp_die( __("<b>خطا:</b>لطفا کد امنیتی را تایید کنید!<p><a href='javascript:history.back()'>« بازگشت</a></p>"));
+	else if (!is_valid_captcha($recaptcha))
+		wp_die( __("<b>Go away Spammer!</b>"));
+}
+if (!is_user_logged_in()) {
+	add_action('pre_comment_on_post', 'verify_google_recaptcha');
+}
+/* End Recaptcha */
