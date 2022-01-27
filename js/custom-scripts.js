e2e2fb6796c64b01cc244853c41b91df5e36c20f
Unco <wizard2070@gmail.com>
2022-01-27 11:37:03 +0330

    styles_for_english_site_v2

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -12,7 +12,6 @@
         });
         /* Notif Banner */
 
-
         // Start Goftino
         if (SpaAjax.isEN !== 'en'){
             !function(){var a=window,d=document;function g(){var g=d.createElement("script"),i="lXpDBP",s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.type="text/javascript",g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();
@@ -30,28 +29,18 @@ jQuery(document).ready(function($){
     toolTips.click(function () {
         $('[data-toggle="tooltip"]').tooltip("hide");
     });
-    // Inits
+    // inits
 
 
     // Add Material icons
     $('.single_add_to_cart_button').html('<i class="ic-cart" style="font-size: 150%"></i>  افزودن به سبد خرید');
     $('.cart_dropdown_link').find('span:first-child').html('<i class="ic-cart my-2"></i>');
 
-
-    // Change breadcrumb trails
-    $('.breadcrumb-trail').find('span.sep').html('<i class="ic-line-left"></i>');
-    $('.trail-begin').find('span[itemprop="name"]').html('<span itemprop="name">صفحه اصلی</span>');
-
-
     // Full-Width shop page
     $('ul.products').removeClass('columns-3').addClass('col-12');
 
-
     // Change Sipaad phone number
     $('#product-14049').find('.woo-price').css('display' , 'none');
-    // $('#product-14049').find('.woo-price').find('strong').html('برای دریافت قیمت با شماره 03132362894 تماس بگیرید');
-    // $('.template-shop').find('.post-14049').find('.price').find('strong').html('برای دریافت قیمت با شماره 03132362894 تماس بگیرید');
-
 
     /* Start Change Shop buttons text temporary */
     $('.template-shop').find('li.type-product').find('.inner_product').find('.show_details_button').css('direction', 'rtl').html('افزودن به سبد خرید<i class="ic-basket mx-2" style="font-size: 115%;font-weight: bold;vertical-align: bottom;"></i>');
@@ -78,10 +67,6 @@ jQuery(document).ready(function($){
     /* End Change Shop buttons text temporary */
 
 
-    // Customize woocommerce product desc
-    //$('.woocommerce-product-details__short-description').before("<p style='font-size: 15px;font-weight: bold;padding: 4px 0;margin: 0;'>ویژگی ها</p>");
-
-
     // fadeout UAP REG SUCCESS MSG
     const uap_reg_success_msg = $('.uap-reg-success-msg');
     setTimeout(function () {
@@ -90,16 +75,25 @@ jQuery(document).ready(function($){
         }
     } , 3000);
 
-
     $('#uap_createuser').find('.optional').siblings('label.uap-labels-register').addClass('optional-label');
 
 
-    // Change header phone info
-    if (SpaAjax.isEN === 'en') $('#header_meta').find('.phone-info').find('span').html('Business Unit: +98-03132362894 - Support Unit: +98-03195016151');
-    else $('#header_meta').find('.phone-info').find('span').html('بازرگانی : ۰۳۱۳۲۳۶۲۸۹۴ |  پشتیبانی : ۰۳۱۹۵۰۱۶۱۵۱');
-
-    $('i.ic-line-right').removeClass('ic-line-right').addClass('ic-line-left');
-    $('.trail-begin').find('span:nth-child(2)').text('dsadad');
+    /* Changes For Multi Language */
+    // header phone info
+    if (SpaAjax.isEN === 'en') {
+        $('#header_meta').find('.phone-info').find('span').html('<i class="ic-phone">&nbsp;&nbsp;+98-03132362894 | +98-03195016151&nbsp;&nbsp;</i>');
+    } else {
+        $('#header_meta').find('.phone-info').find('span').html('بازرگانی : ۰۳۱۳۲۳۶۲۸۹۴ |  پشتیبانی : ۰۳۱۹۵۰۱۶۱۵۱');
+    }
+    // Change breadcrumb trails
+    const breadcrumbTrail = $('.breadcrumb-trail');
+    if (SpaAjax.isEN === 'en') {
+        $('.breadcrumb').find('.trail-begin').find('span[itemprop="name"]').text('Home');
+        breadcrumbTrail.find('span.sep').html('<i class="ic-line-right"></i>');
+    } else {
+        $('.breadcrumb').find('.trail-begin').find('span[itemprop="name"]').text('صفحه اصلی');
+        breadcrumbTrail.find('span.sep').html('<i class="ic-line-left"></i>');
+    }
 
 });
 
